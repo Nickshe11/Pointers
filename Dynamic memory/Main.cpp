a9d3f06a7a8f5e3fa80d97734f3fa9f6669ec969
Nickshe11 <shelobod@gmail.com>
2022-09-10 18:47:27 +0300

    Hometask split and array modification

--- Dynamic memory/Main.cpp
+++ Dynamic memory/Main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int* arr, const int n);
 int* push_back(int* arr, int& n, int value);
+int* push_front(int* arr, int& n, int value);
+int* insert(int* arr, int& n, int value, int index);
+int* pop_back(int* arr, int& n);
+int* pop_front(int* arr, int& n);
+int* erase(int* arr, int& n, int index);
 
 void main()
 {
@@ -15,11 +20,35 @@ void main()
 	FillRand(arr, n);
 	Print(arr, n);
 
-	int value;
-	cout << "Введите добавляемое значение: "; cin >> value;
+	int value, choice, index;
+	//cout << "Введите добавляемое значение: "; cin >> value;
+	cout << "Выберите операцию с массивом:" << endl;
+	cout << "1 - добавление элемента в конец массива" << endl << "2 - Добавление элемента в начало массива" << endl << "3 - добавление элемента в массив по индексу" << endl;
+	cout << "4 - удаление элемента из конца массива" << endl << "5 - удаление элемента из начала массива" << endl << "6 - Удаление значения из массива по указанному индексу" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case(1):
+		cout << "Введите добавляемое значение: "; cin >> value;
+		arr = push_back(arr, n, value); break;
 
-	arr = push_back(arr, n, value);
+	case(2):
+		cout << "Введите добавляемое значение: "; cin >> value;
+		arr = push_front(arr, n, value); break;
 
+	case(3):
+		cout << "Введите добавляемое значение: "; cin >> value;
+		cout << "Введите индекс добавляемого значения: "; cin >> index;
+		arr = insert(arr, n, value, index); break;
+	case(4):
+		arr = pop_back(arr, n); break;
+	case(5):
+		arr= pop_front(arr, n); break;
+	case(6):
+		cout << "Введите индекс удаляемого значения: "; cin >> index;
+		arr = erase(arr, n, index); break;
+	default: cout << "Input error" << endl; break;
+	}
 	Print(arr, n);
 	delete[] arr;
 
@@ -71,5 +100,65 @@ int* push_back(int* arr, int& n, int value)
 	//количество элементов массива увеличивается на 1
 	n++;
 	//7) Mission completed, element added
+	return arr;
+}
+int* push_front(int* arr, int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i <= n; i++)
+	{
+		(i == 0 ? buffer[i] = value : buffer[i] = arr[i - 1]);
+	}
+	delete[]arr;
+	arr = buffer;
+	n++;
+	return arr;
+}
+int* insert(int* arr, int& n, int value, int index)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i <= n; i++)
+	{
+		(i <= (index - 1) ? (i == (index - 1) ? buffer[i] = value : buffer[i] = arr[i]) : buffer[i] = arr[i - 1]);
+	}
+	delete[]arr;
+	arr = buffer;
+	n++;
+	return arr;
+}
+int* pop_back(int* arr, int& n)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n-1; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[]arr;
+	arr = buffer;
+	n--;
+	return arr;
+}
+int* pop_front(int* arr, int& n)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}
+	delete[]arr;
+	arr = buffer;
+	n--;
+	return arr;
+}
+int* erase(int* arr, int& n, int index)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+	{
+		(i <= (index - 1) ? (i == (index - 1) ? (buffer[i] = arr[i + 1]):(buffer[i] = arr[i])) : buffer[i] = arr[i + 1]);
+	}
+	delete[]arr;
+	arr = buffer;
+	n--;
 	return arr;
 }
---
+++ Split/Main.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n = 10;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	int m = n / 2;
+	int* earr = new int[m];
+	int* oarr = new int[m];
+	//Не по заданию, разделение массива на четные и нечетные
+	for (int i = 0; i < n; i++)
+	{
+		(i % 2 == 0 ? earr[i / 2] = arr[i] : oarr[i / 2] = arr[i]);
+	}
+	cout << "Вывод двух производных массивов, разделенных по четным и нечетным индексам" << endl;
+	Print(earr, m);
+	Print(oarr, m);
+	cout << "_____________________________________________" << endl<<endl;
+	int odd=0, even=0;
+	for (int i = 0; i < n; i++)
+	{
+		(arr[i] % 2 == 0 ? even ++ : odd++);
+	}
+	cout << "Вывод двух производных массивов, разделенных на основании четности-нечетности значений" << endl;
+	cout << "even = " << even << "\t" << "odd = " << odd<< endl;
+	int* earr1 = new int[even];
+	int* oarr1 = new int[odd];
+	int ecounter = 0, ocounter = 0;
+	for (int i = 0; i < n; i++)
+	{
+		(arr[i] % 2 == 0 ? (earr1[ecounter] = arr[i], ecounter++) : (oarr1[ocounter] = arr[i], ocounter++));
+	}
+	Print(earr1, even);
+	Print(oarr1, odd);
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr [i] << "\t";
+	}
+	cout << endl;
+}
